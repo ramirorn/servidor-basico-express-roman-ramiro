32d759969ad0b839a38bcc0ab795bc13d7ac9fa4
Ramiro <ramiroroman306@gmail.com>
2025-06-08 10:53:36 -0300

    Se corrigio la sintaxis, se agrego el error 404

--- app.js
+++ app.js
@@ -13,24 +13,47 @@ const items = data.items;
 app.get('/api/characters', (req, res) => {
     console.log('Respuesta a la solicitud de personajes');
     res.setHeader('Content-Type', 'application/json');
-    res.json({items}); // Envia la lista de personajes como respuesta en formato JSON
+    res.json({ items }); // Envia la lista de personajes como respuesta en formato JSON
 });
 // -----------------Respuesta a la solicitud---------------------
 
 // ------------------Obtiene personaje por ID-------------------
 app.get('/api/characters/:id', (req, res) => {
-    console.log(`Obteniendo personaje por ID: ${req.params.id}`)});
-// -----------------Obtiene personaje por ID--------------------
+    console.log(`Obteniendo personaje por ID: ${req.params.id}`)
 
-// Verifica que el ID sea un número
-const id = req.params.id;
-const idNumber = parseInt(id);
+    // Verifica que el ID sea un número
+    const id = req.params.id;
+    const idNumber = parseInt(id);
 
-// Si el ID no es un número, devuelve un error 400
-if (isNaN(idNumber)) {
+    // Si el ID no es un número, devuelve un error 400
+    if (isNaN(idNumber)) {
+        return res.status(400).json({
+            message: 'Invalid parameter: ID must be a number',
+            error: 'Bad Request',
+            statusCode: 400,
+        });
+    }
+
+    // Busca el personaje por ID en el array
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].id === idNumber) {
+            res.setHeader('Content-Type', 'application/json');
+            return res.json(items[i]);
+        }
+    }
+    // Si no se encuentra el personaje, devuelve un error 400
     return res.status(400).json({
-        message: 'Invalid parameter: ID must be a number',
+        message: 'Chacter id not found',
         error: 'Bad Request',
         statusCode: 400,
     });
-}
+});
+
+// En caso de que no se encuentre la ruta solicitada, devuelve un error 404
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Not such file or directory',
+        error: 'Not Found',
+        statusCode: 404,
+    });
+});
